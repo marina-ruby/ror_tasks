076c88ef1b6da7f46405d218f5baae099958def9
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-12-22 12:32:14 +0300

    Chnage factories and file seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,2 @@
 FactoryBot.create_list(:tag, 20)
-
-20.times do
-  author = FactoryBot.create(:author)
-  10.times do
-    article = author.articles.create(FactoryBot.attributes_for(:article))
-    article.comments.create(FactoryBot.attributes_for_list(:comment, rand(0..10)))
-    article.tags << Tag.order("RANDOM()").limit(3)
-  end
-end
+FactoryBot.create_list(:author_with_article, 20)
--- spec/factories/author_factory.rb
+++ spec/factories/author_factory.rb
@@ -5,5 +5,14 @@ FactoryBot.define do
     city { Faker::Address.city }
     address { Faker::Address.street_address }
     date_of_birth { Faker::Date.birthday(18, 85) }
+
+    factory :author_with_article do
+      transient do
+        articles_count 10
+      end
+      after(:create) do |author, evaluator|
+        create_list(:article_with_comment, 10, author: author)
+      end
+    end
   end
 end
