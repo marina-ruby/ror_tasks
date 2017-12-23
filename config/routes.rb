fad29827e5e2463663866533f835bb327dd832ab
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-12-23 08:32:34 +0300

    Return file for view and controller for next task

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   resources :tags
   resources :articles
   resources :authors
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
