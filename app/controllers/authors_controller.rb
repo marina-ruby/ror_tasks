ea77defe1c15887bff41cfee1ce19ec90bc8e3c8
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-12-23 14:28:36 +0300

    Add pagination for author's view

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,10 +2,11 @@ class AuthorsController < ApplicationController
   before_action :set_author, only: [:show, :edit, :update, :destroy]
 
   def index
-    @authors = Author.all
+    @authors = Author.paginate(:page => params[:page], :per_page => 10)
   end
 
   def show
+    @aritcles = Articles.all
   end
 
   def new
