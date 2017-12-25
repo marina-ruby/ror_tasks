17e7b15f591f220eba14be406e0d31e350dbe3df
Bellatrix988 <marina.yurova1996@yandex.ru>
2017-12-25 11:23:39 +0300

    Add path to articles with current tag; add navigation menu

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,61 +2,20 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    # @articles = Article.where("? in tags",params[:tag])
-    @articles = Article.paginate(page: params[:page], per_page: 10)
-                       .order('created_at DESC')
+    if params[:tag]
+      model = Article.joins(:tags).where(tags: { name: params[:tag] })
+    else
+      model = Article
+    end
+    @articles = model.paginate(page: params[:page], per_page: 10)
+                     .order('created_at DESC')
   end
 
   def show
   end
 
-  def new
-    @article = Article.new
-  end
-
-  def edit
-  end
-
-  def create
-    @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
-      else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
     def set_article
       @article = Article.find(params[:id])
-    end
-
-    def article_params
-      params.require(:article).permit(:title, :text)
     end
 end
