9b38bf0863b7e2f2ee542def9a2e72ca46c561a2
Bellatrix988 <marina.yurova1996@yandex.ru>
2018-01-11 15:24:54 +0300

    Fix method for add new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    @article = Article.new(article_params)
+    @article = current_user.articles.create(article_params)
     redirect_to articles_path
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180111080520) do
+ActiveRecord::Schema.define(version: 20180111114650) do
 
   create_table "articles", force: :cascade do |t|
     t.integer "author_id"
